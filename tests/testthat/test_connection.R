2d7bdd2b0c2b24be4650d9cf8d5163103a8096b0
Rasmus Skytte Randløv <rasmusskytteeriksen@gmail.com>
2023-08-14 11:53:25 +0200

    test: changed how conn_drv is pulled from conn_list

--- tests/testthat/test_connection.R
+++ tests/testthat/test_connection.R
@@ -16,8 +16,8 @@ test_that("id() works", { for (conn in conns){
 test_that("close_connection() works", { for (conn in conns) {
   # Identify driver of conn within conn_list
   conn_drv <- sapply(conns, \(.x) identical(.x, conn)) |>
-    (\(.x) `[`(conn_list, .x))() |>
-    unlist()
+    names() |>
+    (\(.) purrr::pluck(conn_list, .))()
 
   conn2 <- get_driver(conn_drv)
 
