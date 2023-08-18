ac9000b4202d54766b74896a256a82d4483cfa85
Rasmus Skytte Randløv <rasmusskytteeriksen@gmail.com>
2023-08-18 12:45:51 +0200

    feat: removing age aggregators from package

--- tests/testthat/test_helpers.R
+++ tests/testthat/test_helpers.R
@@ -4,48 +4,6 @@ test_that("%notin% works", {
 })
 
 
-test_that("age_label() works", {
-  expect_identical(age_labels(c(10, 20, 25, 35)),
-                   c("00-09", "10-19", "20-24", "25-34", "35+"))
-  expect_identical(age_labels(0),
-                   c("0+"))
-  expect_identical(age_labels(c(1, 2, 5)),
-                   c("0-0", "1-1", "2-4", "5+"))
-  expect_identical(age_labels(c(10, 100)),
-                   c("000-009", "010-099", "100+"))
-})
-
-test_that("mg_age_label() works", { for (conn in conns){
-  expect_false(exists("mg_age_labels") && packageVersion("mg") > "1.0")
-  # Time to fully deprecate mg_age_labels
-}})
-
-
-test_that("aggregate_age() works", {
-  age_cuts <- c(10, 20, 25, 35)
-  age <- c(5, 9, 10, 15, 20, 25, 26, 30, 35, 40)
-
-  expect_identical(aggregate_age(age, age_cuts),
-                   factor(c(1, 1, 2, 2, 3, 4, 4, 4, 5, 5),
-                          labels = c("00-09", "10-19", "20-24", "25-34", "35+")))
-})
-
-
-test_that("aggregate_age_sql() works", { for (conn in conns){
-  age_cuts <- c(10, 20, 25, 35)
-  age <- c(5, 9, 10, 15, 20, 25, 26, 30, 35, 40)
-
-  q <- dplyr::copy_to(conn, data.frame(age), overwrite = TRUE)
-
-  expect_identical(q |> dplyr::mutate(age = !!aggregate_age_sql(age, age_cuts)) |> dplyr::pull(age),
-                   c("00-09", "00-09", "10-19", "10-19", "20-24", "25-34", "25-34", "25-34", "35+", "35+"))
-
-  expect_identical(q |> dplyr::mutate(age = !!aggregate_age_sql("age", age_cuts)) |> dplyr::pull(age),
-                   c("00-09", "00-09", "10-19", "10-19", "20-24", "25-34", "25-34", "25-34", "35+", "35+"))
-
-}})
-
-
 test_that("nrow() works", { for (conn in conns){
   x <- get_table(conn, "__mtcars")
 
