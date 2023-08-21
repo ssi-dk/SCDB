510afaa4f6a5a56af1fce26558ed8cac3df63abb
Rasmus Skytte Randløv <rasmusskytteeriksen@gmail.com>
2023-08-21 13:49:06 +0200

    test: restructuring the last db_maintenance tests

--- tests/testthat/test-update_snapshot.R
+++ tests/testthat/test-update_snapshot.R
@@ -192,38 +192,3 @@ test_that("update_snapshot() works", { for (conn in conns) { # nolint: brace_lin
 
   if (DBI::dbExistsTable(conn, id("test.mg_tmp1", conn))) DBI::dbRemoveTable(conn, id("test.mg_tmp1", conn))
 }})
-
-
-test_that("filter_keys() works", { for (conn in conns) { # nolint: brace_linter
-
-  x <- get_table(conn, "__mtcars")
-
-  expect_equal(x,
-               x |> filter_keys(NULL))
-
-  filter <- x |> utils::head(10) |> dplyr::select(name)
-  expect_equal(x |>
-                 dplyr::filter(name %in% !!pull(filter, name)) |>
-                 dplyr::collect(),
-               x |>
-                 filter_keys(filter) |>
-                 dplyr::collect())
-
-  filter <- x |> utils::head(10) |> dplyr::select(vs, am) |> dplyr::distinct()
-  expect_equal(x |>
-                 dplyr::inner_join(filter, by = c("vs", "am")) |>
-                 dplyr::collect(),
-               x |>
-                 filter_keys(filter) |>
-                 dplyr::collect())
-
-  # Filtering with null means no filtering is done
-  m <- mtcars
-  row.names(m) <- NULL
-  filter <- NULL
-  expect_identical(filter_keys(m, filter), m)
-
-  # Filtering by vs = 0
-  filter <- data.frame(vs = 0)
-  expect_mapequal(filter_keys(m, filter), dplyr::filter(m, vs == 0))
-}})
--- tests/testthat/test_db_manipulating_functions.R
+++ tests/testthat/test_db_manipulating_functions.R
@@ -143,3 +143,37 @@ test_that("slice_time() works", { for (conn in conns) { # nolint: brace_linter
   expect_equal(xx |> slice_time("2022-06-01") |> nrow(), 20)
   expect_equal(xx |> slice_time("2022-06-15") |> nrow(), nrow(mtcars))
 }})
+
+test_that("filter_keys() works", { for (conn in conns) { # nolint: brace_linter
+
+  x <- get_table(conn, "__mtcars")
+
+  expect_equal(x,
+               x |> filter_keys(NULL))
+
+  filter <- x |> utils::head(10) |> dplyr::select(name)
+  expect_equal(x |>
+                 dplyr::filter(name %in% !!pull(filter, name)) |>
+                 dplyr::collect(),
+               x |>
+                 filter_keys(filter) |>
+                 dplyr::collect())
+
+  filter <- x |> utils::head(10) |> dplyr::select(vs, am) |> dplyr::distinct()
+  expect_equal(x |>
+                 dplyr::inner_join(filter, by = c("vs", "am")) |>
+                 dplyr::collect(),
+               x |>
+                 filter_keys(filter) |>
+                 dplyr::collect())
+
+  # Filtering with null means no filtering is done
+  m <- mtcars
+  row.names(m) <- NULL
+  filter <- NULL
+  expect_identical(filter_keys(m, filter), m)
+
+  # Filtering by vs = 0
+  filter <- data.frame(vs = 0)
+  expect_mapequal(filter_keys(m, filter), dplyr::filter(m, vs == 0))
+}})
