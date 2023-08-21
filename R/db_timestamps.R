30c43b031ddb082b733bf045169f9274004fe8c1
Rasmus Skytte Randløv <rasmusskytteeriksen@gmail.com>
2023-08-21 14:02:53 +0200

    fix: Fixed db_timestamp mishandling POSIXt objects

--- R/db_timestamps.R
+++ R/db_timestamps.R
@@ -2,23 +2,22 @@
 #' @name db_timestamp
 #' @param timestamp The timestamp to be transformed to the DB type. Can be character.
 #' @param conn A `DBIConnection` to the DB where the timestamp should be stored
-#' @export
 db_timestamp <- function(timestamp, conn) {
   UseMethod("db_timestamp", conn)
 }
 
 #' @rdname db_timestamp
-#' @export
 db_timestamp.default <- function(timestamp, conn) {
+  if (inherits(timestamp, "POSIXt")) timestamp <- format(timestamp)
   return(dbplyr::translate_sql(as.POSIXct(!!timestamp), con = conn))
 }
 
 #' @rdname db_timestamp
-#' @export
 db_timestamp.SQLiteConnection <- function(timestamp, conn) {
   if (is.na(timestamp)) {
     dbplyr::translate_sql(NA_character_, con = conn)
   } else {
+    if (inherits(timestamp, "POSIXt")) timestamp <- format(timestamp)
     dbplyr::translate_sql(!!strftime(timestamp), con = conn)
   }
 }
