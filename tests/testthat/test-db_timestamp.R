1a7fffaa5f21cf3ca5826d693f0947268db5dd1c
Marcus Munch <MMGR@ssi.dk>
2023-08-22 13:43:17 +0200

    Increase coverage in several files (#4)
    * test: Test db_timestamp.default
    * test: Test that Logger stops if existing file is found
    * test: Test that get_table() returns list of tables if none given
    
    * chore: typo and linting

--- tests/testthat/test-db_timestamp.R
+++ tests/testthat/test-db_timestamp.R
@@ -6,4 +6,10 @@ test_that("db_timestamp produce consistent results", { for (conn in conns) { # n
     db_timestamp(ts_posix, conn),
     db_timestamp(ts_str, conn)
   )
+
+  # Test default fallback
+  expect_identical(
+    db_timestamp.default(ts_posix, conn = NULL),
+    db_timestamp.default(ts_str, conn = NULL)
+  )
 }})
