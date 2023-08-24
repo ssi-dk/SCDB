e5e78fcdb12b5d29c5c21808db9ab9c758d4ded2
Rasmus Skytte Randløv <rasmusskytteeriksen@gmail.com>
2023-08-24 10:33:42 +0200

    test: test that .Rd files have \Value
    Release SCDB 0.1.0 #10

--- tests/testthat/test-return.R
+++ tests/testthat/test-return.R
@@ -33,3 +33,20 @@ test_that("Exported code has @return", {
     }
   }
 })
+
+
+test_that(r"{.Rd files have \Value}", {
+  pkg_dir <- stringr::str_remove(getwd(), "/tests/testthat")
+
+  rd_files <- list.files(path = file.path(pkg_dir, "man"), pattern = r"{\.[Rr][Dd]$}", full.names = TRUE)
+
+  files_to_check <- c(rd_files) |>
+    purrr::discard(~ stringr::str_detect(.x, "SCDB-package"))
+
+  for (file in files_to_check) {
+    lines <- readLines(file, warn = FALSE)
+
+    has_value <- any(grepl(r"{\Value}", lines, perl = TRUE))
+    expect_true(has_value, label = paste("File:", file))
+  }
+})
