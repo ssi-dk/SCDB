74bbcf37500462734450373219d42d4c28db50be
Rasmus Skytte Randløv <rasmusskytteeriksen@gmail.com>
2023-08-24 13:08:22 +0200

    test: change test for examples to look only at .Rd files
    Release SCDB 0.1.0 #10

--- tests/testthat/test-examples.R
+++ tests/testthat/test-examples.R
@@ -1,35 +1,15 @@
-test_that("Exported code has @examples", {
+test_that(r"{.Rd files have \examples}", {
   pkg_dir <- stringr::str_remove(getwd(), "/tests/testthat")
 
-  r_files  <- list.files(path = file.path(pkg_dir, "R"),   pattern = r"{\.[Rr]$}",     full.names = TRUE)
+  rd_files <- list.files(path = file.path(pkg_dir, "man"), pattern = r"{\.[Rr][Dd]$}", full.names = TRUE)
 
-  files_to_check <- r_files
+  files_to_check <- c(rd_files) |>
+    purrr::discard(~ stringr::str_detect(.x, "SCDB-package"))
 
   for (file in files_to_check) {
     lines <- readLines(file, warn = FALSE)
 
-    code_blocks <- lines |>
-      stringr::str_detect("^#'") |>
-      (\(.) . & !c(F, .[-length(.)]))() |>
-      cumsum() |>
-      (\(.) split(lines, .))()
-
-    # Truncate
-    code_blocks <- purrr::map(code_blocks, ~ purrr::discard(., cumprod(stringr::str_detect(., "^$")) == 1))
-
-    # Look for @export tag
-    exported_code_blocks <- purrr::keep(code_blocks, ~ any(stringr::str_detect(., "@export")))
-
-    # Look for missing @examples tag
-    no_examples <- purrr::discard(exported_code_blocks, ~ any(stringr::str_detect(., "@example")))
-
-    # Report issues
-    for (block in no_examples) {
-      function_with_issue <- purrr::keep(block, ~ stringr::str_detect(., r"{(?<= <-) function\(}")) |>
-        stringr::str_extract(r"{^[\w\.%`]*(?= <-)}")
-
-      expect_true(any(stringr::str_detect(block, "@example")),
-                  label = glue::glue("{function_with_issue} is exported but has no @example(s)"))
-    }
+    has_example <- any(grepl(r"{\\examples}", lines))
+    expect_true(has_example, label = paste("File:", file))
   }
 })
