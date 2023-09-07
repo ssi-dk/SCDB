fbc62bdd32d4649cce5c4b068a870d416eaa5000
Marcus Munch <MMGR@ssi.dk>
2023-09-07 13:33:01 +0200

    fix: detect schema with no tables

--- R/get_schema.R
+++ R/get_schema.R
@@ -57,9 +57,24 @@ schema_exists <- function(conn, schema) {
   checkmate::assert_class(conn, "DBIConnection")
   checkmate::assert_character(schema)
 
+  if (inherits(conn, "SQLiteConnection")) return(FALSE)
+
   objs <- DBI::dbListObjects(conn)
   matches <- sapply(objs$table, \(.x) methods::slot(.x, "name")) |>
     (\(.x) names(.x) == "schema" & .x == schema)()
 
-  return(any(matches))
+  if (any(matches)) return(TRUE)
+
+  tryCatch({
+    DBI::dbCreateTable(
+      conn,
+      name = DBI::Id(schema = schema, table = "SCDB_schema_test"),
+      fields = data.frame(name = character()),
+      temporary = FALSE
+    )
+
+    DBI::dbRemoveTable(conn, DBI::Id(schema = schema, table = "SCDB_schema_test"))
+    TRUE
+  },
+  error = function(e) FALSE)
 }
