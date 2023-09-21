e9685e834bd61b40eb53ae92aad486981b17f190
Marcus Munch Grünewald <MMGR@SSI.dk>
2023-09-21 06:41:36 +0200

    test: add separate test for conn = NULL

--- tests/testthat/test-db_timestamp.R
+++ tests/testthat/test-db_timestamp.R
@@ -7,9 +7,14 @@ test_that("db_timestamp produce consistent results", { for (conn in conns) { # n
     db_timestamp(ts_str, conn)
   )
 
+  expect_identical(
+    db_timestamp(ts_posix, conn = NULL),
+    db_timestamp(ts_str, conn = NULL)
+  )
+
   # Test default fallback
   expect_identical(
-    db_timestamp.default(ts_posix, conn = NULL),
-    db_timestamp.default(ts_str, conn = NULL)
+    db_timestamp.default(ts_posix, conn = conn),
+    db_timestamp.default(ts_str, conn = conn)
   )
 }})
