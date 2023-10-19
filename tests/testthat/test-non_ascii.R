819a14bfd5f5a27a63aa3a32bf9549128abb9636
Rasmus Skytte Randløv <rske@ssi.dk>
2023-10-19 11:19:55 +0200

    test(.Rd files): handle path differences between check(), test() and Github

--- tests/testthat/test-non_ascii.R
+++ tests/testthat/test-non_ascii.R
@@ -1,18 +1,67 @@
-test_that("Code contains no non-ascii characters", {
-  pkg_dir <- stringr::str_remove(getwd(), "/tests/testthat")
+test_that("Code contains no non-ASCII characters", {
 
-  r_files  <- list.files(path = file.path(pkg_dir, "R"),   pattern = r"{\.[Rr]$}",     full.names = TRUE)
-  rd_files <- list.files(path = file.path(pkg_dir, "man"), pattern = r"{\.[Rr][Dd]$}", full.names = TRUE)
+  # Load .Rd files based on environment
+  # When using R-CMD-Check the deployment is different from when using devtools::test().
+  # Deployment on Github is also different from running R-CMD-Check locally
+  # Here we need to read directly from a .rdx database stored in the "help" folder
+  # If we are testing locally, we read from the "man" folder
+  # Note that `devtools::test()`, `devtools::check()` and GitHub workflows all have different
+  # folder structures during testing, so we need to account for these differences
 
-  files_to_check <- c(r_files, rd_files) |>
-    purrr::discard(~ stringr::str_detect(.x, "SCDB-package"))
+  # Platform independent regex search to find root folder
+  path_regex <- stringr::str_replace(r"{(.*\/\w*(.Rcheck)?)\/.*(?=\/testthat)}", "/", .Platform$file.sep)
+  pkg_path <- stringr::str_extract(getwd(), path_regex, group = 1)
 
-  for (file in files_to_check) {
-    lines <- readLines(file, warn = FALSE)
-    has_non_ascii <- any(grepl(r"{[^\x00-\x7f]}", lines, perl = TRUE))
-    if (has_non_ascii) {
-      print(grepl(r"{[^\x00-\x7f]}", lines, perl = TRUE))
-    }
-    expect_false(has_non_ascii, label = paste("File:", file))
+  # If path contains .Rcheck, we need to add the package name to the path
+  pkg_path <- stringr::str_replace(pkg_path, r"{(\w*)(\.Rcheck)}", paste0("\\1\\2", .Platform$file.sep, "\\1"))
+
+  # Look for the source of .Rd files
+  help_dir <- file.path(pkg_path, "help")
+  man_dir  <- file.path(pkg_path, "man")
+
+  if (checkmate::test_directory_exists(help_dir)) {
+
+    rdx_file <- purrr::keep(dir(help_dir, full.names = TRUE), ~ stringr::str_detect(., ".rdx$"))
+    rd_envir <- new.env()
+    lazyLoad(stringr::str_remove(rdx_file, ".rdx$"), envir = rd_envir)
+    rd_names <- ls(rd_envir)
+    rd_files <- purrr::map(rd_names, ~ as.character(eval(purrr::pluck(rd_envir, .))))
+    names(rd_files) <- paste0(rd_names, ".Rd")
+
+  } else if (checkmate::test_directory_exists(man_dir)) {
+
+    rd_paths <- purrr::keep(dir(man_dir, full.names = TRUE), ~ stringr::str_detect(., ".[Rr][Dd]$"))
+    rd_files <- purrr::map(rd_paths, readLines)
+    names(rd_files) <- purrr::map_chr(rd_paths, basename)
+
+  } else {
+
+    stop(".Rd files could not be located")
+
+  }
+
+  # Skip the "*-package.Rd" file
+  rd_files <- rd_files[!stringr::str_detect(names(rd_files), "-package.[Rr][Dd]$")]
+
+  r_dir <- file.path(pkg_path, "R")
+
+  if (checkmate::test_directory_exists(r_dir)) {
+
+    r_paths <- purrr::keep(dir(r_dir, full.names = TRUE), ~ stringr::str_detect(., ".[Rr]$"))
+    r_files <- purrr::map(r_paths, readLines)
+    names(r_paths) <- purrr::map_chr(r_paths, basename)
+
+  } else {
+
+    stop(".R files could not be located")
+
+  }
+
+  files_to_check <- c(r_files, rd_files)
+
+  # Check the files
+  for (file_id in seq_along(files_to_check)) {
+    has_non_ascii <- purrr::some(files_to_check[[file_id]], ~ stringr::str_detect(., r"{[^\x00-\x7f]}"))
+    expect_false(has_non_ascii, label = paste("File:", names(files_to_check)[[file_id]]))
   }
 })
--- tests/testthat/test-return.R
+++ tests/testthat/test-return.R
@@ -1,15 +1,52 @@
-test_that(r"{.Rd files have \value}", {
-  pkg_dir <- stringr::str_remove(getwd(), "/tests/testthat")
+test_that(r"{.Rd files have \Value}", {
 
-  rd_files <- list.files(path = file.path(pkg_dir, "man"), pattern = r"{\.[Rr][Dd]$}", full.names = TRUE)
+  # Load .Rd files based on environment
+  # When using R-CMD-Check the deployment is different from when using devtools::test().
+  # Deployment on Github is also different from running R-CMD-Check locally
+  # Here we need to read directly from a .rdx database stored in the "help" folder
+  # If we are testing locally, we read from the "man" folder
+  # Note that `devtools::test()`, `devtools::check()` and GitHub workflows all have different
+  # folder structures during testing, so we need to account for these differences
 
-  files_to_check <- c(rd_files) |>
-    purrr::discard(~ stringr::str_detect(.x, "SCDB-package"))
+  # Platform independent regex search to find root folder
+  path_regex <- stringr::str_replace(r"{(.*\/\w*(.Rcheck)?)\/.*(?=\/testthat)}", "/", .Platform$file.sep)
+  pkg_path <- stringr::str_extract(getwd(), path_regex, group = 1)
 
-  for (file in files_to_check) {
-    lines <- readLines(file, warn = FALSE)
+  # If path contains .Rcheck, we need to add the package name to the path
+  pkg_path <- stringr::str_replace(pkg_path, r"{(\w*)(\.Rcheck)}", paste0("\\1\\2", .Platform$file.sep, "\\1"))
 
-    has_value <- any(stringr::str_detect(lines, r"{\\value}"))
-    expect_true(has_value, label = paste("File:", file))
+  # Look for the source of .Rd files
+  help_dir <- file.path(pkg_path, "help")
+  man_dir  <- file.path(pkg_path, "man")
+
+  if (checkmate::test_directory_exists(help_dir)) {
+
+    rdx_file <- purrr::keep(dir(help_dir, full.names = TRUE), ~ stringr::str_detect(., ".rdx$"))
+    rd_envir <- new.env()
+    lazyLoad(stringr::str_remove(rdx_file, ".rdx$"), envir = rd_envir)
+    rd_names <- ls(rd_envir)
+    rd_files <- purrr::map(rd_names, ~ as.character(eval(purrr::pluck(rd_envir, .))))
+    names(rd_files) <- paste0(rd_names, ".Rd")
+
+  } else if (checkmate::test_directory_exists(man_dir)) {
+
+    rd_paths <- purrr::keep(dir(man_dir, full.names = TRUE), ~ stringr::str_detect(., ".[Rr][Dd]$"))
+    rd_files <- purrr::map(rd_paths, readLines)
+    names(rd_files) <- purrr::map_chr(rd_paths, basename)
+
+  } else {
+
+    stop(".Rd files could not be located")
+
+  }
+
+
+  # Skip the "*-package.Rd" file
+  rd_files <- rd_files[!stringr::str_detect(names(rd_files), "-package.[Rr][Dd]$")]
+
+  # Check renaming
+  for (rd_id in seq_along(rd_files)) {
+    has_value <- any(stringr::str_detect(rd_files[[rd_id]], r"{\\value}"))
+    expect_true(has_value, label = paste("File:", names(rd_files)[[rd_id]]))
   }
 })
