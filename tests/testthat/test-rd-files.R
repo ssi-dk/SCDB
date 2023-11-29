95b82c98b1ce31e47238f4ae7dcf3ba851dcf9c4
Marcus Munch Grünewald <mmgr@ssi.dk>
2023-11-29 09:45:29 +0100

    tests: Simplify package path retrieval

--- tests/testthat/test-rd-files.R
+++ tests/testthat/test-rd-files.R
@@ -8,14 +8,9 @@ test_field_in_documentation <- function(field) {
   # Note that `devtools::test()`, `devtools::check()` and GitHub workflows all have different
   # folder structures during testing, so we need to account for these differences
 
-  # Platform independent regex search to find root folder
-  path_regex <- stringr::str_replace(r"{(.*\/\w*(.Rcheck)?)\/.*(?=\/testthat)}", "/", .Platform$file.sep)
-  pkg_path <- stringr::str_extract(getwd(), path_regex, group = 1)
-
-  # If path contains .Rcheck, we need to add the package name to the path
-  pkg_path <- stringr::str_replace(pkg_path, r"{(\w*)(\.Rcheck)}", paste0("\\1\\2", .Platform$file.sep, "\\1"))
-
   # Look for the source of .Rd files
+  pkg_path <- devtools::as.package(".")$path
+
   help_dir <- file.path(pkg_path, "help")
   man_dir  <- file.path(pkg_path, "man")
 
@@ -43,6 +38,8 @@ test_field_in_documentation <- function(field) {
 
   # Skip the "*-package.Rd" file
   rd_files <- rd_files[!stringr::str_detect(names(rd_files), "-package.[Rr][Dd]$")]
+
+  message("Found ", length(rd_files), " Rd files")
 
   # Check renaming
   for (rd_id in seq_along(rd_files)) {
