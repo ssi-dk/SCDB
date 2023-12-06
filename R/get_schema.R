6bb728858b896f130efc9d51f2a8530fd642bef0
Marcus Munch Grünewald <mmgr@ssi.dk>
2023-12-06 15:14:19 +0100

    docs: (Greatly) expand documentation for `get_schema`

--- R/get_schema.R
+++ R/get_schema.R
@@ -1,7 +1,28 @@
 #' Get the current schema of a database-related objects
 #'
 #' @param .x The object from which to retrieve a schema
-#' @return The current schema name, but defaults to "prod" instead of "public"
+#' @return
+#' For `DBIConnection` objects, the current schema of the connection. See [Default schema][default schema] for more
+#'
+#' For `tbl_dbi` objects, the schema as retrieved from the lazy_query.
+#' If the lazy_query does not specify a schema, `NA` is returned.
+#' Note that lazy queries are sensitive to server-side changes and may therefore return entirely different tables
+#' if changes are made server-side.
+#'
+#' @section Default schema:
+#'
+#' In some backends, it is possible to modify settings so that when a schema is not explicitly stated in a query,
+#' the backend searches for the table in this schema by default.
+#' For Postgres databases, this can be shown with `SELECT CURRENT_SCHEMA()` (defaults to `public`) and modified with
+#' `SET search_path TO { schema }`.
+#'
+#' For SQLite databases, a `temp` schema for temporary tables always exists as well as a `main` schema for permanent
+#' tables.
+#' Additional databases may be attached to the connection with a named schema, but as the attachment must be made after
+#' the connection is established, `get_schema` will never return any of these, as the default schema will always be
+#' one of `main` or `temp` (if `main` contains no tables).
+#' A return value of `NA` means that no tables exist within the connection and no default schema therefore exists.
+#'
 #' @examples
 #' conn <- get_connection(drv = RSQLite::SQLite())
 #'
