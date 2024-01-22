55dd157bcee8590ef8299d16785bf68fe428f3fc
Rasmus Skytte Randløv <rasmusskytteeriksen@gmail.com>
2024-01-22 13:34:05 +0100

    test(id): update test to the new specific behavior

--- tests/testthat/test-connection.R
+++ tests/testthat/test-connection.R
@@ -19,37 +19,72 @@ test_that("get_connection notifies if connection fails", {
 })
 
 
-test_that("id() works", {
+test_that("id() works for character input without implied schema", {
   for (conn in get_test_conns()) {
 
-    expect_identical(id("test_mtcars"), DBI::Id(table = "test_mtcars"))
-    expect_identical(id("test.mtcars"), DBI::Id(schema = "test", table = "mtcars"))
+    # Without schema, we expect:
 
-    expect_identical(id("test.mtcars", conn = conn, allow_table_only = FALSE),
-                     DBI::Id(schema = "test", table = "mtcars"))
+    # ... no change of no conn is given
+    expect_identical(id("test_mtcars"), DBI::Id(table = "test_mtcars"))
+
+    # .. the defaults schema if conn is given
+    expect_identical(id("test_mtcars", conn), DBI::Id(schema = SCDB::get_schema(conn), table = "test_mtcars"))
 
     DBI::dbDisconnect(conn)
   }
 })
 
 
-test_that('id() returns table = "schema.table" if schema does not exist', {
+test_that("id() works for character input with implied schema", {
+  # With schema we expect the implied schema.table to be resolved:
+
+  # ... when no conn is given, we naively assume schema.table holds true
+  expect_identical(id("test.mtcars"), DBI::Id(schema = "test", table = "mtcars"))
+
+
   for (conn in get_test_conns()) {
 
-    while (TRUE) {
-      schema_name <- paste(sample(letters, size = 16, replace = TRUE), collapse = "")
-      if (schema_exists(conn, schema_name)) next
+    # ... when conn is given, we check if implied schema exists.
+    # NOTE: All testing connections should have the schema "test" (except SQLite without attached schemas)
+    # therefore, in almost all cases, we shold resolve the schema correctly (except the SQLite case above)
+    if (inherits(conn, "SQLiteConnection") && !schema_exists(conn, "test")) {
+      expect_identical(id("test.mtcars", conn), DBI::Id(schema = "main", table = "test.mtcars"))
+    } else {
+      expect_identical(id("test.mtcars", conn), DBI::Id(schema = "test", table = "mtcars"))
+    }
 
+    DBI::dbDisconnect(conn)
+  }
+})
+
+
+test_that("id() works for character input with implied schema when schema does not exist", {
+  for (conn in get_test_conns()) {
+
+    # Generate schema that does not exist
+    k <- 0
+    while (TRUE && k < 100) {
+      invalid_schema_name <- paste(sample(letters, size = 16, replace = TRUE), collapse = "")
+      k <- k + 1
+      if (schema_exists(conn, invalid_schema_name)) next
       break
     }
 
-    table_name <- paste(schema_name, "mtcars", sep = ".")
+    if (k < 100) {
 
-    expect_identical(id(table_name, conn, allow_table_only = TRUE),
-                     DBI::Id(table = table_name))
+      table_name <- paste(invalid_schema_name, "mtcars", sep = ".")
 
-    expect_identical(id(table_name, conn, allow_table_only = FALSE),
-                     DBI::Id(schema = schema_name, table = "mtcars"))
+      # When schema does not exist and allow_table_only is TRUE, the schema should be the default schema
+      expect_identical(id(table_name, conn = conn, allow_table_only = TRUE),
+                       DBI::Id(schema = get_schema(conn), table = table_name))
+
+      # When schema does not exist and allow_table_only is FALSE, the schema should be as implied
+      expect_identical(id(table_name, conn = conn, allow_table_only = FALSE),
+                       DBI::Id(schema = invalid_schema_name, table = "mtcars"))
+
+    } else {
+      warning("Non-existing schema could not be generated!")
+    }
 
     DBI::dbDisconnect(conn)
 
@@ -59,7 +94,21 @@ test_that('id() returns table = "schema.table" if schema does not exist', {
 })
 
 
-test_that("id() is consistent for DBI connections", {
+test_that("id() works for DBI::Id inputs", {
+  for (conn in get_test_conns()) {
+
+    # When passing an Id without a schema, id should enrich the Id with the default schema
+    expect_identical(
+      id(DBI::Id(table = "mtcars"), conn),
+      DBI::Id(schema = get_schema(conn), table = "mtcars")
+    )
+
+    DBI::dbDisconnect(conn)
+  }
+})
+
+
+test_that("id() is consistent for tbl_dbi inputs", {
   for (conn in get_test_conns()) {
 
     expectation <- id(dplyr::tbl(conn, id("test.mtcars", conn), check_from = FALSE))
--- tests/testthat/test-get_table.R
+++ tests/testthat/test-get_table.R
@@ -8,50 +8,129 @@ test_that("get_tables() works", {
       tidyr::unite("db_table_name", "schema", "table", sep = ".", na.rm = TRUE) |>
       dplyr::pull(db_table_name)
 
-    expect_true("test.mtcars" %in% db_table_names)
-    expect_true("__mtcars" %in% db_table_names)
+
+    # Check for the existence of "test.mtcars" and "__mtcars" (added during test setup)
+    # For SQLite connections, we don't always have the "test" schema, so we check for its existence
+    # and use default schema if it does not exist.
+    table_1 <- paste(c(switch(!schema_exists(conn, "test"), get_schema(conn)), "test.mtcars"), collapse = ".")
+    table_2 <- paste(c(get_schema(conn), "__mtcars"), collapse = ".")
+
+    checkmate::expect_subset(c(table_1, table_2), db_table_names)
 
     # Now test with pattern
     db_table_names <- get_tables(conn, pattern = "__mt") |>
       tidyr::unite("db_table_name", "schema", "table", sep = ".", na.rm = TRUE) |>
       dplyr::pull(db_table_name)
 
-
-    expect_false("test.mtcars" %in% db_table_names)
-    expect_true("__mtcars" %in% db_table_names)
-
-    DBI::dbDisconnect(conn)
-  }
-})
-
-
-test_that("table_exists() works", {
-  for (conn in get_test_conns()) {
-
-    expect_true(table_exists(conn, "__mtcars"))
-    expect_false(table_exists(conn, "__mctars"))
-
-    expect_true(table_exists(conn, DBI::Id(table = "__mtcars")))
-    expect_false(table_exists(conn, DBI::Id(table = "__mctars")))
-
-    expect_true(table_exists(conn, "test.mtcars"))
-    expect_false(table_exists(conn, "tset.mtcars"))
-
-    expect_true(table_exists(conn, DBI::Id(schema = "test", table = "mtcars")))
-    expect_false(table_exists(conn, DBI::Id(schema = "tset", table = "mtcars")))
+    expect_false(table_1 %in% db_table_names)
+    expect_true(table_2 %in% db_table_names)
 
     DBI::dbDisconnect(conn)
   }
 })
 
 
-test_that("get_table() works", {
+test_that("table_exists() works for default schema", {
   for (conn in get_test_conns()) {
+
+    # Generate table in default schema that does not exist
+    k <- 0
+    while (TRUE && k < 100) {
+      invalid_table_name <- paste(sample(letters, size = 16, replace = TRUE), collapse = "")
+      k <- k + 1
+      if (DBI::dbExistsTable(conn, id(invalid_table_name, conn))) next
+      break
+    }
+
+    if (k < 100) {
+
+      # Without explicit schema, table_exists assumes default schema
+      expect_true(table_exists(conn, "__mtcars"))
+      expect_false(table_exists(conn, invalid_table_name))
+
+      expect_true(table_exists(conn, DBI::Id(table = "__mtcars")))
+      expect_false(table_exists(conn, DBI::Id(table = invalid_table_name)))
+
+      # Using the default schema should therefore yield the same results
+      expect_true(table_exists(conn, paste(get_schema(conn), "__mtcars", sep = ".")))
+      expect_false(table_exists(conn, paste(get_schema(conn), invalid_table_name, sep = ".")))
+
+      expect_true(table_exists(conn, DBI::Id(schema = get_schema(conn), table = "__mtcars")))
+      expect_false(table_exists(conn, DBI::Id(schema = get_schema(conn), table = invalid_table_name)))
+
+    } else {
+      warning("Non-existing table in default schema could not be generated!")
+    }
+
+    DBI::dbDisconnect(conn)
+  }
+})
+
+
+test_that("table_exists() works for non-default schema", {
+  for (conn in get_test_conns()) {
+
+    # Generate schema that does not exist
+    k <- 0
+    while (TRUE && k < 100) {
+      invalid_schema_name <- paste(sample(letters, size = 16, replace = TRUE), collapse = "")
+      k <- k + 1
+      if (schema_exists(conn, invalid_schema_name)) next
+      break
+    }
+
+    if (k < 100) {
+
+      # With a given schema, table_exists should still determine existence correctly
+      # NOTE: # Not all connections support schemas
+
+      # Character inputs
+      if (schema_exists(conn, "test")) {
+        expect_true(!schema_exists(conn, "test") || table_exists(conn, "test.mtcars"))
+      } else {
+        expect_false(table_exists(conn, "test.mtcars"))
+      }
+      expect_false(table_exists(conn, paste(invalid_schema_name, "mtcars", sep = ".")))
+
+
+      # DBI::Id inputs
+      if (schema_exists(conn, "test")) {
+        expect_true(table_exists(conn, DBI::Id(schema = "test", table = "mtcars")))
+      } else {
+        expect_false(table_exists(conn, DBI::Id(schema = "test", table = "mtcars")))
+      }
+      expect_false(table_exists(conn, DBI::Id(schema = invalid_schema_name, table = "mtcars")))
+
+    } else {
+      warning("Non-existing schema could not be generated!")
+    }
+
+    DBI::dbDisconnect(conn)
+  }
+})
+
+
+test_that("get_table returns list of tables if no table is requested", {
+  for (conn in get_test_conns()) {
+
+    expect_message(
+      get_table(conn),
+      regexp = "Select one of the following tables:"
+    )
+
+    DBI::dbDisconnect(conn)
+  }
+})
+
+
+test_that("get_table() works when tables exist", {
+  for (conn in get_test_conns()) {
+
 
     mtcars_t <- tibble::tibble(mtcars |> dplyr::mutate(name = rownames(mtcars)))
 
     # Lets try different ways to read __mtcars
-    expect_mapequal(get_table(conn, "__mtcars") |> dplyr::collect(), mtcars_t)
+    expect_mapequal(get_table(conn, "__mtcars")  |> dplyr::collect(), mtcars_t)
     expect_equal(get_table(conn, id("__mtcars")) |> dplyr::collect(), mtcars_t)
     t <- "__mtcars"
     expect_equal(get_table(conn, t) |> dplyr::collect(), mtcars_t)
@@ -66,24 +145,80 @@ test_that("get_table() works", {
     t <- id("test.mtcars", conn)
     expect_equal(get_table(conn, t) |> dplyr::collect(), mtcars_t)
 
-    # Test some malformed inputs
-    expect_error(get_table(conn, "tset.mtcars"),     regexp = "Table tset.mtcars is not found!")
-    expect_error(get_table(conn, id("tset.mtcars", conn)), regexp = "Table tset.mtcars is not found!")
-    t <- "tset.mtcars"
-    expect_error(get_table(conn, t), regexp = "Table tset.mtcars is not found!")
-    t <- id("tset.mtcars", conn)
-    expect_error(get_table(conn, t), regexp = "Table tset.mtcars is not found!")
+    DBI::dbDisconnect(conn)
+  }
+})
+
+
+test_that("get_table() works when tables does not exist in default schema", {
+  for (conn in get_test_conns()) {
+
+    # Generate table in default schema that does not exist
+    k <- 0
+    while (TRUE && k < 100) {
+      invalid_table_name <- paste(sample(letters, size = 16, replace = TRUE), collapse = "")
+      k <- k + 1
+      if (DBI::dbExistsTable(conn, id(invalid_table_name, conn))) next
+      break
+    }
+
+    if (k < 100) {
+
+      expect_error(
+        get_table(conn, invalid_table_name),
+        regexp = glue::glue("Table {capture.output(print(id(invalid_table_name, conn)))} could not be found!")
+      )
+      expect_error(
+        get_table(conn, id(invalid_table_name, conn)),
+        regexp = glue::glue("Table {capture.output(print(id(invalid_table_name, conn)))} could not be found!")
+      )
+      expect_error(
+        get_table(conn, id(invalid_table_name)),
+        regexp = glue::glue("Table {capture.output(print(id(invalid_table_name, conn)))} could not be found!")
+      )
+
+    } else {
+      warning("Non-existing table in default schema could not be generated!")
+    }
 
     DBI::dbDisconnect(conn)
   }
 })
 
-test_that("get_table returns list of tables if no table is requested", {
+
+test_that("get_table() works when tables does not exist in non existing schema", {
   for (conn in get_test_conns()) {
 
-    expect_message(get_table(conn),
-      regexp = "Select one of the following tables:"
-    )
+    # Generate schema that does not exist
+    k <- 0
+    while (TRUE && k < 100) {
+      invalid_schema_name <- paste(sample(letters, size = 16, replace = TRUE), collapse = "")
+      k <- k + 1
+      if (schema_exists(conn, invalid_schema_name)) next
+      break
+    }
+
+    if (k < 100) {
+
+      # Test some malformed inputs
+      invalid_table_name <- paste(invalid_schema_name, "mtcars", sep = ".")
+
+      expect_error(
+        get_table(conn, invalid_table_name),
+        regexp = glue::glue("Table {capture.output(print(id(invalid_table_name, conn)))} could not be found!")
+      )
+      expect_error(
+        get_table(conn, id(invalid_table_name, conn)),
+        regexp = glue::glue("Table {capture.output(print(id(invalid_table_name, conn)))} could not be found!")
+      )
+      expect_error(
+        get_table(conn, id(invalid_table_name)),
+        regexp = glue::glue("Table {capture.output(print(id(invalid_table_name)))} could not be found!")
+      )
+
+    } else {
+      warning("Non-existing schema could not be generated!")
+    }
 
     DBI::dbDisconnect(conn)
   }
