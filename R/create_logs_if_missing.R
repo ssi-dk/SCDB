e2c49344c24964afaeadce06a52deeae43076fa9
Rasmus Skytte Randløv <rasmusskytteeriksen@gmail.com>
2024-01-26 17:24:46 +0100

    chore: move create_logs_if_missing to separate files

---
+++ R/create_logs_if_missing.R
@@ -0,0 +1,34 @@
+#' Create a table with the SCDB log structure if it does not exists
+#' @template conn
+#' @param log_table A specification of where the logs should exist ("schema.table")
+#' @return A tbl_dbi with the generated (or existing) log table
+#' @examples
+#' conn <- get_connection(drv = RSQLite::SQLite())
+#' log_table_id <- id("test.logs", conn = conn, allow_table_only = TRUE)
+#'
+#' create_logs_if_missing(log_table_id, conn)
+#'
+#' close_connection(conn)
+#' @export
+create_logs_if_missing <- function(log_table, conn) {
+
+  checkmate::assert_class(conn, "DBIConnection")
+
+  if (!table_exists(conn, log_table)) {
+    log_signature <- data.frame(date = as.POSIXct(NA),
+                                schema = NA_character_,
+                                table = NA_character_,
+                                n_insertions = NA_integer_,
+                                n_deactivations = NA_integer_,
+                                start_time = as.POSIXct(NA),
+                                end_time = as.POSIXct(NA),
+                                duration = NA_character_,
+                                success = NA,
+                                message = NA_character_,
+                                log_file = NA_character_)
+
+    DBI::dbCreateTable(conn, id(log_table, conn), log_signature)
+  }
+
+  return(dplyr::tbl(conn, id(log_table, conn), check_from = FALSE))
+}
