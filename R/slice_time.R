be2c66be2eb183b65d49d91fe22893527dc110a4
Rasmus Skytte Randløv <rasmusskytteeriksen@gmail.com>
2024-02-05 09:39:35 +0100

    feat(slice_time): don't require unquoted from/until_ts

--- R/slice_time.R
+++ R/slice_time.R
@@ -1,38 +1,43 @@
 #' Slices a data object based on time / date
 #'
 #' @template .data
-#' @param slice_ts The time / date to slice by
-#' @param from_ts  The name of the column in .data specifying valid from time (note: must be unquoted)
-#' @param until_ts The name of the column in .data specifying valid until time (note: must be unquoted)
+#' @param slice_ts (`POSIXct(1)`, `Date(1)`, or `character(1)`)\cr
+#'   The time / date to slice by
+#' @param from_ts,until_ts (`character(1)`)\cr
+#'   The name of the columns in .data specifying valid from and valid until time
 #' @template .data_return
 #' @examples
-#' conn <- get_connection(drv = RSQLite::SQLite())
+#'   conn <- get_connection(drv = RSQLite::SQLite())
 #'
-#' m <- mtcars |>
-#'   dplyr::mutate(from_ts = dplyr::if_else(dplyr::row_number() > 10,
-#'                                          as.Date("2020-01-01"),
-#'                                          as.Date("2021-01-01")),
-#'                 until_ts = as.Date(NA))
+#'   m <- mtcars |>
+#'     dplyr::mutate(
+#'       "from_ts" = dplyr::if_else(dplyr::row_number() > 10,
+#'                                  as.Date("2020-01-01"),
+#'                                  as.Date("2021-01-01")),
+#'       "until_ts" = as.Date(NA))
 #'
-#' dplyr::copy_to(conn, m, name = "mtcars", temporary = FALSE)
+#'   dplyr::copy_to(conn, m, name = "mtcars", temporary = FALSE)
 #'
-#' q <- dplyr::tbl(conn, id("mtcars", conn))
+#'   q <- dplyr::tbl(conn, id("mtcars", conn))
 #'
-#' nrow(slice_time(q, "2020-01-01")) # 10
-#' nrow(slice_time(q, "2021-01-01")) # nrow(mtcars)
+#'   nrow(slice_time(q, "2020-01-01")) # 10
+#'   nrow(slice_time(q, "2021-01-01")) # nrow(mtcars)
 #'
-#' close_connection(conn)
+#'   close_connection(conn)
 #' @export
-slice_time <- function(.data, slice_ts, from_ts = from_ts, until_ts = until_ts) {
+slice_time <- function(.data, slice_ts, from_ts = "from_ts", until_ts = "until_ts") {
 
   # Check arguments
-  assert_data_like(.data)
-  assert_timestamp_like(slice_ts)
+  coll <- checkmate::makeAssertCollection()
+  assert_data_like(.data, add = coll)
+  assert_timestamp_like(slice_ts, add = coll)
+  checkmate::assert_character(from_ts, add = coll)
+  checkmate::assert_character(until_ts, add = coll)
+  checkmate::reportAssertions(coll)
 
-  from_ts  <- dplyr::enquo(from_ts)
-  until_ts <- dplyr::enquo(until_ts)
   .data <- .data |>
-    dplyr::filter(is.na({{until_ts}}) | slice_ts < {{until_ts}},
-                  {{from_ts}} <= slice_ts)
+    dplyr::filter(dplyr::if_any(tidyselect::all_of(from_ts), ~ . <= !!slice_ts),
+                  dplyr::if_any(tidyselect::all_of(until_ts), ~ is.na(.) | !!slice_ts < .))
+
   return(.data)
 }
