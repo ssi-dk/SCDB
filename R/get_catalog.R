650c3ea27da3708f20e9fa4375f8f00ffc862f31
Rasmus Skytte Randløv <rasmusskytteeriksen@gmail.com>
2024-02-13 10:02:07 +0100

    docs(get_schema, get_catalog): Combine and improve docs

--- R/get_catalog.R
+++ R/get_catalog.R
@@ -1,21 +1,11 @@
-#' Get the current catalog of a database-related objects
-#'
-#' @name get_catalog
-#' @inheritParams get_schema
+#' @rdname get_schema
 #' @return
-#' For `get_catalog.Microsoft SQL Server`, the current database context of the connection or "tempdb" if
-#' `temporary = TRUE`.
+#'   * For `get_catalog.Microsoft SQL Server`, the current database context of the connection or "tempdb" if
+#'   `temporary = TRUE`.
 #'
-#' For `get_catalog.*`, `NULL` is returned.
-#' @examples
-#' conn <- get_connection(drv = RSQLite::SQLite())
+#'   * For `get_schema.tbl_dbi` the catalog is determined via `id()`.
 #'
-#' dplyr::copy_to(conn, mtcars, name = "mtcars", temporary = FALSE)
-#'
-#' get_catalog(conn)
-#' get_catalog(get_table(conn, id("mtcars", conn = conn)))
-#'
-#' close_connection(conn)
+#'   * For `get_catalog.\*`, `NULL` is returned.
 #' @export
 get_catalog <- function(obj, ...) {
   UseMethod("get_catalog")
@@ -31,7 +21,7 @@ get_catalog.Id <- function(obj, ...) {
   return(purrr::pluck(obj@name, "catalog"))
 }
 
-#' @rdname get_catalog
+#' @rdname get_schema
 #' @export
 `get_catalog.Microsoft SQL Server` <- function(obj, temporary = FALSE, ...) {
   checkmate::assert_logical(temporary)
