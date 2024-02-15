65013cee9db21f55c1baa3538acec19a712f71b9
Rasmus Skytte Randløv <rasmusskytteeriksen@gmail.com>
2024-02-15 12:12:18 +0100

    test(getTableSignature): Add tests and update function expectations

--- tests/testthat/test-getTableSignature.R
+++ tests/testthat/test-getTableSignature.R
@@ -1,6 +1,276 @@
-test_that("getTableSignature() generates a signature for NULL connections", {
-  expect_identical(
-    lapply(cars, class),
-    as.list(getTableSignature(cars, conn = NULL))
-  )
-})
+# Generate test datasets with different data types
+
+# One that follows the structure in update_snapshot()
+data_update_snapsnot <- data.frame(
+  "date"      = Sys.Date(),
+  "POSIXct"   = Sys.time(),
+  "character" = "test",
+  "integer"   = as.integer(1),
+  "numeric"   = as.numeric(1),
+  "logical"   = TRUE,
+  # .. and our special columns
+  "checksum"  = "test",
+  "from_ts"   = Sys.time(),
+  "until_ts"  = Sys.time()
+)
+
+# One that has the special columns of update_snapshot(), but not at the end
+data_random <- data.frame(
+  "date"      = Sys.Date(),
+  "POSIXct"   = Sys.time(),
+  "character" = "test",
+  # .. Our special columns, but not at the end
+  "checksum"  = "test",
+  "from_ts"   = Sys.time(),
+  "until_ts"  = Sys.time(),
+  # ..
+  "integer"   = as.integer(1),
+  "numeric"   = as.numeric(1),
+  "logical"   = TRUE
+)
+
+for (conn in c(c(NULL), get_test_conns())) {
+
+  if (is.null(conn)) {
+    test_that("getTableSignature() matches local R types for NULL connections", {
+      for (data in c(data_update_snapsnot, data_random)) {
+        expect_identical(
+          getTableSignature(data, conn),
+          purrr::map(data, ~ purrr::pluck(., class, 1)) # eg. class POSIXct has two elemments, we choose first
+        )
+      }
+    })
+  }
+
+
+  if (inherits(conn, "SQLiteConnection")) {
+    test_that("getTableSignature() generates signature for update_snapshot() (SQLiteConnection)", {
+      expect_identical(
+        getTableSignature(data_update_snapsnot, conn),
+        c(
+          "date"      = "DATE",
+          "POSIXct"   = "TIMESTAMP",
+          "character" = "TEXT",
+          "integer"   = "INT",
+          "numeric"   = "DOUBLE",
+          "logical"   = "SMALLINT",
+          # ..
+          "checksum"  = "TEXT",
+          "from_ts"   = "TIMESTAMP",
+          "until_ts"  = "TIMESTAMP"
+        )
+      )
+    })
+  }
+
+  if (inherits(conn, "PqConnection")) {
+    test_that("getTableSignature() generates signature for update_snapshot() (PqConnection)", {
+      expect_identical(
+        getTableSignature(data_update_snapsnot, conn),
+        c(
+          "date"      = "DATE",
+          "POSIXct"   = "TIMESTAMP",
+          "character" = "TEXT",
+          "integer"   = "INTEGER",
+          "numeric"   = "NUMERIC",
+          "logical"   = "BOOLEAN",
+          # ..
+          "checksum"  = "VARCHAR(32)",
+          "from_ts"   = "TIMESTAMP",
+          "until_ts"  = "TIMESTAMP"
+        )
+      )
+    })
+  }
+
+  if (inherits(conn, "Microsoft SQL Server")) {
+    test_that("getTableSignature() generates signature for update_snapshot() (Microsoft SQL Server)", {
+      expect_identical(
+        getTableSignature(data_update_snapsnot, conn),
+        c(
+          "date"      = "DATE",
+          "POSIXct"   = "DATETIME2",
+          "character" = "TEXT",
+          "integer"   = "INT",
+          "numeric"   = "FLOAT",
+          "logical"   = "BOOLEAN",
+          # ..
+          "checksum"  = "VARCHAR(40)",
+          "from_ts"   = "DATETIME2",
+          "until_ts"  = "DATETIME2"
+        )
+      )
+    })
+  }
+
+
+  if (inherits(conn, "SQLiteConnection")) {
+    test_that("getTableSignature() generates signature for random data (SQLiteConnection)", {
+      expect_identical(
+        getTableSignature(data_random, conn),
+        c(
+          "date"      = "DATE",
+          "POSIXct"   = "TIMESTAMP",
+          "character" = "TEXT",
+          # ..
+          "checksum"  = "TEXT",
+          "from_ts"   = "TIMESTAMP",
+          "until_ts"  = "TIMESTAMP",
+          # ..
+          "integer"   = "INT",
+          "numeric"   = "DOUBLE",
+          "logical"   = "SMALLINT"
+        )
+      )
+    })
+  }
+
+  if (inherits(conn, "PqConnection")) {
+    test_that("getTableSignature() generates signature for random data (PqConnection)", {
+      expect_identical(
+        getTableSignature(data_update_snapsnot, conn),
+        c(
+          "date"      = "DATE",
+          "POSIXct"   = "TIMESTAMP",
+          "character" = "TEXT",
+          # ..
+          "checksum"  = "TEXT",
+          "from_ts"   = "TIMESTAMP",
+          "until_ts"  = "TIMESTAMP",
+          # ..
+          "integer"   = "INTEGER",
+          "numeric"   = "NUMERIC",
+          "logical"   = "BOOLEAN"
+        )
+      )
+    })
+  }
+
+  if (inherits(conn, "Microsoft SQL Server")) {
+    test_that("getTableSignature() generates signature for random data (Microsoft SQL Server)", {
+      expect_identical(
+        getTableSignature(data_update_snapsnot, conn),
+        c(
+          "date"      = "DATE",
+          "POSIXct"   = "DATETIME2",
+          "character" = "TEXT",
+          # ..
+          "checksum"  = "TEXT",
+          "from_ts"   = "DATETIME2",
+          "until_ts"  = "DATETIME2",
+          # ..
+          "integer"   = "INT",
+          "numeric"   = "FLOAT",
+          "logical"   = "BOOLEAN"
+        )
+      )
+    })
+  }
+
+
+  if (inherits(conn, "SQLiteConnection")) {
+    test_that("getTableSignature() generates signature for random data on remote (SQLiteConnection)", {
+      expect_identical(
+        getTableSignature(dplyr::copy_to(conn, data_random), conn),
+        c(
+          "date"      = "DOUBLE",  # By copying to SQLite and back, information is changed by
+          "POSIXct"   = "DOUBLE",  # dbplyr / DBI so data types are now similar, but different.
+          "character" = "TEXT",    # Dates and timestamps which are normally stored in SQLite
+          # ..                     # as internally TEXT are now converted to DOUBLE
+          "checksum"  = "TEXT",    # Logical, which have the "SMALLINT" type are now "INT"
+          "from_ts"   = "DOUBLE",  # In the next test, we check that this conversion is consistent
+          "until_ts"  = "DOUBLE",  # for the user on the local R side.
+          # ..
+          "integer"   = "INT",
+          "numeric"   = "DOUBLE",
+          "logical"   = "INT"
+        )
+      )
+    })
+  }
+
+  if (inherits(conn, "PqConnection")) {
+    test_that("getTableSignature() generates signature for random data on remote (PqConnection)", {
+      expect_identical(
+        getTableSignature(dplyr::copy_to(conn, data_random), conn),
+        c(
+          "date"      = "DATE",
+          "POSIXct"   = "TIMESTAMP",
+          "character" = "TEXT",
+          # ..
+          "checksum"  = "TEXT",
+          "from_ts"   = "TIMESTAMP",
+          "until_ts"  = "TIMESTAMP",
+          # ..
+          "integer"   = "INTEGER",
+          "numeric"   = "NUMERIC",
+          "logical"   = "BOOLEAN"
+        )
+      )
+    })
+  }
+
+  if (inherits(conn, "Microsoft SQL Server")) {
+    test_that("getTableSignature() generates signature for random data on remote (Microsoft SQL Server)", {
+      expect_identical(
+        getTableSignature(dplyr::copy_to(conn, data_random), conn),
+        c(
+          "date"      = "DATE",
+          "POSIXct"   = "DATETIME2",
+          "character" = "TEXT",
+          # ..
+          "checksum"  = "TEXT",
+          "from_ts"   = "DATETIME2",
+          "until_ts"  = "DATETIME2",
+          # ..
+          "integer"   = "INT",
+          "numeric"   = "FLOAT",
+          "logical"   = "BOOLEAN"
+        )
+      )
+    })
+  }
+
+
+  if (!is.null(conn)) {
+    test_that(glue::glue("getTableSignature() generates consistent data types ({class(conn)})"), {
+      # This tests that the data types are consistent when copying to a remote table with getTableSignature().
+      # We first copy the data to a remote table, then copy that table to another remote table on the same connection.
+      # The
+      remote_data_1 <- dplyr::copy_to(
+        conn,
+        data_random,
+        name = "remote_data_1",
+        types = getTableSignature(data_random, conn)
+      )
+      remote_data_2 <- dplyr::copy_to(
+        conn,
+        remote_data_1,
+        name = "remote_data_2",
+        types = getTableSignature(remote_data_1, conn)
+      )
+
+      # The table signatures are not always the same (eg. SQLiteConnection).
+      if (inherits(conn, "SQLiteConnection")) {
+        expect_false(identical( # In leui of expect_not_identical
+          getTableSignature(data_random, conn),
+          getTableSignature(remote_data_1, conn)
+        ))
+        expect_identical(
+          names(getTableSignature(data_random, conn)),
+          names(getTableSignature(remote_data_1, conn))
+        )
+      } else {
+        expect_identical(
+          getTableSignature(data_random, conn),
+          getTableSignature(remote_data_1, conn)
+        )
+      }
+
+      # But the data, when transfered locally, should be the same
+      expect_identical(dplyr::collect(remote_data_2), dplyr::collect(remote_data_1))
+    })
+  }
+
+  if (!is.null(conn)) connection_clean_up(conn)
+}
