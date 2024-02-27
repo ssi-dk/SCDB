3a32ae1f6102c2a5a56569349e3b185c7ddae072
Rasmus Skytte Randløv <rske@ssi.dk>
2024-02-27 09:19:42 +0100

    fix(getTableSignature): Import DBIConnection class

--- R/getTableSignature.R
+++ R/getTableSignature.R
@@ -12,9 +12,10 @@ methods::setGeneric("getTableSignature",
                     function(.data, conn = NULL) standardGeneric("getTableSignature"),
                     signature = "conn")
 
+#' @importClassesFrom DBI DBIConnection
 methods::setMethod("getTableSignature", "DBIConnection", function(.data, conn) {
 
-  # Retrive the translated data types
+  # Retrieve the translated data types
   signature <- as.list(DBI::dbDataType(conn, dplyr::collect(utils::head(.data, 0))))
 
   # Define the column types to be updated based on backend class
