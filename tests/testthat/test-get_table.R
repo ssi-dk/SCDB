cc83c49a37ee065a1fd486e821eed74d32d79d9b
Rasmus Skytte Randløv <rasmusskytteeriksen@gmail.com>
2024-02-29 13:01:27 +0100

    chore(get_table): Remove orphaned tests

--- tests/testthat/test-get_table.R
+++ tests/testthat/test-get_table.R
@@ -1,62 +1,4 @@
-test_that("get_tables() works", {
-  for (conn in get_test_conns()) {
-
-    tables <- get_tables(conn)
-    expect_s3_class(tables, "data.frame")
-
-    db_table_names <- tables |>
-      tidyr::unite("db_table_name", "schema", "table", sep = ".", na.rm = TRUE) |>
-      dplyr::pull(db_table_name)
-
-
-    # Check for the existence of "test.mtcars" and "__mtcars" (added during test setup)
-    # For SQLite connections, we don't always have the "test" schema, so we check for its existence
-    # and use default schema if it does not exist.
-    table_1 <- paste(c(switch(!schema_exists(conn, "test"), get_schema(conn)), "test.mtcars"), collapse = ".")
-    table_2 <- paste(c(get_schema(conn), "__mtcars"), collapse = ".")
-
-    # We should not get tables twice
-    expect_setequal(db_table_names, unique(db_table_names))
-
-    # Our test tables should be present
-    checkmate::expect_subset(c(table_1, table_2), db_table_names)
-
-
-    # Now test with pattern
-    db_table_names <- get_tables(conn, pattern = "__mt") |>
-      tidyr::unite("db_table_name", "schema", "table", sep = ".", na.rm = TRUE) |>
-      dplyr::pull(db_table_name)
-
-    # We should not get tables twice
-    expect_setequal(db_table_names, unique(db_table_names))
-
-    # Our test table that matches the pattern should be present
-    expect_false(table_1 %in% db_table_names)
-    expect_true(table_2 %in% db_table_names)
-
-
-    # Now test with temporary tables
-    tmp <- dplyr::copy_to(conn, mtcars, "__mtcars_2", temporary = TRUE)
-    tmp_id <- id(tmp)
-    tmp_name <- paste(tmp_id@name["schema"], tmp_id@name["table"], sep = ".")
-
-    db_table_names <- get_tables(conn, show_temporary = TRUE) |>
-      tidyr::unite("db_table_name", "schema", "table", sep = ".", na.rm = TRUE) |>
-      dplyr::pull(db_table_name)
-
-
-    # We should not get tables twice
-    expect_setequal(db_table_names, unique(db_table_names))
-
-    # Our test tables should be present
-    checkmate::expect_subset(c(table_1, table_2, tmp_name), db_table_names)
-
-    connection_clean_up(conn)
-  }
-})
-
-
-test_that("get_table returns list of tables if no table is requested", {
+test_that("get_table() returns list of tables if no table is requested", {
   for (conn in get_test_conns()) {
 
     expect_message(
@@ -71,7 +13,6 @@ test_that("get_table returns list of tables if no table is requested", {
 
 test_that("get_table() works when tables exist", {
   for (conn in get_test_conns()) {
-
 
     mtcars_t <- tibble::tibble(mtcars |> dplyr::mutate(name = rownames(mtcars)))
 
