fc478de9a29e76e4c16565c06e6ecccf4a937b2e
RasmusSkytte <rske@ssi.dk>
2024-10-17 12:04:29 +0000

    chore: synced file(s) with ssi-dk/AEF-DDF

--- tests/spelling.R
+++ tests/spelling.R
@@ -1,3 +1,7 @@
-if (requireNamespace("spelling", quietly = TRUE)) {
-  spelling::spell_check_test(vignettes = TRUE, error = FALSE, skip_on_cran = TRUE)
+if (rlang::is_installed("spelling") && !as.logical(Sys.getenv("CI", unset = FALSE))) {
+  spelling::spell_check_test(
+    vignettes = TRUE,
+    error = FALSE,
+    skip_on_cran = TRUE
+  )
 }
