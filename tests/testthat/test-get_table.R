d318d5533ba922b6c3dec51301618d0ce5150314
Rasmus Skytte Randløv <rasmusskytteeriksen@gmail.com>
2025-10-24 13:36:40 +0200

    debug type casting

--- tests/testthat/test-get_table.R
+++ tests/testthat/test-get_table.R
@@ -14,18 +14,18 @@ test_that("get_table() returns list of tables if no table is requested", {
 test_that("get_table() works when tables/view exist", {
   for (conn in get_test_conns()) {
 
-    print('dplyr::show_query(get_table(conn, "__mtcars"))')
-    print(dplyr::show_query(get_table(conn, "__mtcars")))
+    print('dplyr::show_query(get_table(conn, test.mtcars))')
+    print(dplyr::show_query(get_table(conn, test.mtcars)))
 
-    print('get_table(conn, "__mtcars")')
-    print(get_table(conn, "__mtcars"))
+    print('get_table(conn, test.mtcars)')
+    print(get_table(conn, test.mtcars))
 
-    print('dplyr::collect(get_table(conn, "__mtcars"))')
-    print(dplyr::collect(get_table(conn, "__mtcars")))
+    print('dplyr::collect(get_table(conn, test.mtcars))')
+    print(dplyr::collect(get_table(conn, test.mtcars)))
 
     f <- getMethod("dbGetQuery", signature(conn="JDBCConnection", statement="character"))@.Data
-    print('f("SELECT * FROM FROM GITHUB_CI."__mtcars";)')
-    print(f(conn@jdbc_conn, "SELECT * FROM FROM GITHUB_CI.\"__mtcars\";"))
+    print('f("SELECT * FROM FROM \"test\".\"mtcars\";)')
+    print(f(conn@jdbc_conn, "SELECT * FROM \"test\".\"mtcars\";"))
 
     print("??")
 
