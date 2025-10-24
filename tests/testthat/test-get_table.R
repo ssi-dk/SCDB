076080b0ddf670eba5dad44d2ab79c46ae6c48ff
Rasmus Skytte Randløv <rasmusskytteeriksen@gmail.com>
2025-10-24 12:31:02 +0200

    debug type casting

--- tests/testthat/test-get_table.R
+++ tests/testthat/test-get_table.R
@@ -14,10 +14,16 @@ test_that("get_table() returns list of tables if no table is requested", {
 test_that("get_table() works when tables/view exist", {
   for (conn in get_test_conns()) {
 
+    print('dplyr::show_query(get_table(conn, "__mtcars"))')
+    print(dplyr::show_query(get_table(conn, "__mtcars")))
+
     print('get_table(conn, "__mtcars")')
     print(get_table(conn, "__mtcars"))
+
     print('dplyr::collect(get_table(conn, "__mtcars"))')
     print(dplyr::collect(get_table(conn, "__mtcars")))
+
+
 
     mtcars_t <- tibble::tibble(mtcars %>% dplyr::mutate(name = rownames(mtcars)))
 
