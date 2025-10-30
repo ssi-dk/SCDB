a33c79592fd7317d91a30697afc2a973a861b50c
Rasmus Skytte Randløv <rasmusskytteeriksen@gmail.com>
2025-10-30 20:48:04 +0000

    test(db_timestamp): Test different timestamps map to the same value

--- tests/testthat/test-db_timestamp.R
+++ tests/testthat/test-db_timestamp.R
@@ -1,4 +1,4 @@
-test_that("db_timestamp produce consistent results", {
+test_that("`db_timestamp()` produces consistent results", {
   for (conn in get_test_conns()) {
     ts_posix <- Sys.time()
     ts_str <- format(ts_posix)
@@ -17,6 +17,50 @@ test_that("db_timestamp produce consistent results", {
     expect_identical(
       db_timestamp.default(ts_posix, conn = conn),
       db_timestamp.default(ts_str, conn = conn)
+    )
+
+    connection_clean_up(conn)
+  }
+})
+
+
+test_that("`db_timestamp()` maps identically for different inputs", {
+
+  # Create list of slice_ts to test
+  slice_tss <- list(
+    "Date" = Sys.Date(),
+    "character Date" = as.character(Sys.Date()),
+    "Integer Date" = as.Date(as.integer(Sys.Date())),
+    "POSIXct" = lubridate::floor_date(Sys.time(), unit = "day"),
+    "character timestamp" = format(lubridate::floor_date(Sys.time(), unit = "day"))
+  )
+
+  for (conn in get_test_conns()) {
+
+    # Get all combinations and generate SQL
+    test_results <- tidyr::expand_grid(
+      type_1 = slice_tss,
+      type_2 = slice_tss
+    ) |>
+      purrr::pmap_lgl(
+        ~ DBI::dbGetQuery(
+          conn,
+          glue::glue("SELECT {db_timestamp(..1, conn)} = {db_timestamp(..2, conn)}")
+        )[[1]]
+      )
+
+    labels <- tidyr::expand_grid(
+      type_1 = names(slice_tss),
+      type_2 = names(slice_tss)
+    ) |>
+      purrr::pmap_chr(~ glue::glue("{.x} / {.y}"))
+
+    names(test_results) <- labels
+
+    # All db_timestamps should map to the same value
+    expect_identical(
+      test_results,
+      stats::setNames(rep(TRUE, length(labels)), labels)
     )
 
     connection_clean_up(conn)
