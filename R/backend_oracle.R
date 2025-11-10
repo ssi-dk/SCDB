c8993b55c6bd070cfa29f7f8ed196bde23a87082
Rasmus Skytte Randløv <rske@ssi.dk>
2025-11-10 13:26:16 +0100

    debug: disable dbQuoteIdentifier

--- R/backend_oracle.R
+++ R/backend_oracle.R
@@ -162,54 +162,54 @@ setMethod(
   }
 )
 
-#' @importMethodsFrom DBI dbQuoteIdentifier
-#' @exportMethod dbQuoteIdentifier
-setMethod(
-  "dbQuoteIdentifier",
-  signature(
-    conn = "OracleJdbc",
-    x = "character"
-  ),
-  function(conn, x, ...) {
-    x <- enc2utf8(x)
+# #' @importMethodsFrom DBI dbQuoteIdentifier
+# #' @exportMethod dbQuoteIdentifier
+# setMethod(
+#   "dbQuoteIdentifier",
+#   signature(
+#     conn = "OracleJdbc",
+#     x = "character"
+#   ),
+#   function(conn, x, ...) {
+#     x <- enc2utf8(x)
 
-    reserved_words <- c("DATE", "NUMBER", "VARCHAR")
+#     reserved_words <- c("DATE", "NUMBER", "VARCHAR")
 
-    needs_escape <- (grepl("^[a-zA-Z_]", x) & toupper(x) != x) |  tolower(x) %in% reserved_words
+#     needs_escape <- (grepl("^[a-zA-Z_]", x) & toupper(x) != x) |  tolower(x) %in% reserved_words
 
-    x[needs_escape] <- paste0("\"", gsub("\"", "\"\"", x[needs_escape]), "\"")
+#     x[needs_escape] <- paste0("\"", gsub("\"", "\"\"", x[needs_escape]), "\"")
 
-    return(DBI::SQL(x, names = names(x)))
-  }
-)
+#     return(DBI::SQL(x, names = names(x)))
+#   }
+# )
 
-#' @importMethodsFrom DBI dbQuoteIdentifier
-#' @exportMethod dbQuoteIdentifier
-setMethod(
-  "dbQuoteIdentifier",
-  signature(
-    conn = "OracleJdbc",
-    x = "SQL"
-  ),
-  function(conn, x, ...) {
-    return(x) # Remove ambiguity (also assume already quoted)
-  }
-)
+# #' @importMethodsFrom DBI dbQuoteIdentifier
+# #' @exportMethod dbQuoteIdentifier
+# setMethod(
+#   "dbQuoteIdentifier",
+#   signature(
+#     conn = "OracleJdbc",
+#     x = "SQL"
+#   ),
+#   function(conn, x, ...) {
+#     return(x) # Remove ambiguity (also assume already quoted)
+#   }
+# )
 
-#' @importMethodsFrom DBI dbQuoteIdentifier
-#' @exportMethod dbQuoteIdentifier
-setMethod(
-  "dbQuoteIdentifier",
-  signature(
-    conn = "OracleJdbc",
-    x = "Id"
-  ),
-  function(conn, x, ...) {
+# #' @importMethodsFrom DBI dbQuoteIdentifier
+# #' @exportMethod dbQuoteIdentifier
+# setMethod(
+#   "dbQuoteIdentifier",
+#   signature(
+#     conn = "OracleJdbc",
+#     x = "Id"
+#   ),
+#   function(conn, x, ...) {
 
-    # For `Id`, run on each non-NA element
-    return(DBI::SQL(paste0(DBI::dbQuoteIdentifier(conn@jdbc_conn, purrr::discard(x@name, is.na)), collapse = ".")))
-  }
-)
+#     # For `Id`, run on each non-NA element
+#     return(DBI::SQL(paste0(DBI::dbQuoteIdentifier(conn@jdbc_conn, purrr::discard(x@name, is.na)), collapse = ".")))
+#   }
+# )
 
 
 
