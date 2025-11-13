3c63510da4c9e3a3d96478e99a037f084b383346
Rasmus Skytte Randløv <rske@ssi.dk>
2025-11-13 15:03:37 +0100

    test: Create test for `defer_db_cleanup`

---
+++ tests/testthat/test-defer_db_cleanup.R
@@ -0,0 +1,19 @@
+test_that("`defer_db_cleanup()` deletes tables", {
+  for (conn in get_test_conns()) {
+    table <- dplyr::copy_to(conn, mtcars, "__mtcars_defer_db_cleanup")
+    table_id <- id(table, conn)
+
+    # Table exists
+    expect_true(DBI::dbExistsTable(conn, table_id))
+
+    # Marking for deletion does not delete the table
+    defer_db_cleanup(table_id)
+    expect_true(DBI::dbExistsTable(conn, table_id))
+
+    # Manually triggering deletion deletes the table
+    withr::deferred_run()
+    expect_false(DBI::dbExistsTable(conn, table_id))
+
+    connection_clean_up(conn)
+  }
+})
