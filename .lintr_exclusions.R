280f79478f39da7a386080f1275412cfbf4ce63e
Rasmus Skytte Randløv <rasmusskytteeriksen@gmail.com>
2025-12-03 09:57:40 +0100

    feat(.lintr): Add exclusion for pipe_consistency_linter (to .R and .Rmd)

--- .lintr_exclusions.R
+++ .lintr_exclusions.R
@@ -1,6 +1,11 @@
-list(
-  "/*" = list(
-    # We primarily use %>% but shared dev files use |>
-    "pipe_consistency_linter" = Inf
-  )
+files <- list.files(
+  path = c("R", "tests", "vignettes", "data-raw"),
+  recursive = TRUE,
+  full.names = TRUE,
+  pattern = "\\.(R|Rmd)$"
+)
+
+stats::setNames(
+  rep(list(list("pipe_consistency_linter" = Inf)), length(files)),
+  files
 )
