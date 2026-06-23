3bc3ed4a06b4d8ac11986da61df4d3186d33d2bb
Rasmus Skytte Randløv <rasmusskytteeriksen@gmail.com>
2026-06-23 11:17:05 +0200

    debug defer_db_cleanup test

--- tests/testthat/test-defer_db_cleanup.R
+++ tests/testthat/test-defer_db_cleanup.R
@@ -5,6 +5,12 @@ test_that("`defer_db_cleanup()` deletes tables", {
 
     # Table exists
     expect_true(DBI::dbExistsTable(conn, "__mtcars_defer_db_cleanup"))
+    expect_true(
+      DBI::dbExistsTable(
+        conn,
+        DBI::Id(schema = "GITHUB_CI", table = "__mtcars_defer_db_cleanup")
+      )
+    )
     expect_true(DBI::dbExistsTable(conn, table_id))
 
     # Marking for deletion does not delete the table
