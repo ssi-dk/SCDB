1b38a01fca3102565e6338ae4444d5047129085b
Rasmus Skytte Randløv <rasmusskytteeriksen@gmail.com>
2026-06-23 10:23:42 +0200

    write temporary table in defer_db_cleanup

--- tests/testthat/test-defer_db_cleanup.R
+++ tests/testthat/test-defer_db_cleanup.R
@@ -1,8 +1,7 @@
 test_that("`defer_db_cleanup()` deletes tables", {
   for (conn in get_test_conns()) {
-    table <- dplyr::copy_to(conn, mtcars, "__mtcars_defer_db_cleanup")
+    table <- dplyr::copy_to(conn, mtcars, "__mtcars_defer_db_cleanup", temporary = FALSE)
     table_id <- id(table, conn)
-    stop(table_id)
 
     # Table exists
     expect_true(DBI::dbExistsTable(conn, "__mtcars_defer_db_cleanup"))
